1836615dbc57c4fd5e310c57a347a630f63bcf64
bmitchel <b980m824@ku.edu>
2014-11-27 02:49:10 -0600

    Finished Login connection

--- dist/signin/form-signin.php
+++ dist/signin/form-signin.php
@@ -1,4 +1,25 @@
-<?php
-	echo $_REQUEST['inputEmail'];
-	echo $_REQUEST['inputPassword'];
+<?phpOA
+	$username = $_REQUEST['username'];
+	$password = $_REQUEST['password'];
+
+	$queryPatientUsername = "SELECT Patient_Id FROM Patient WHERE Username = '$username'";
+	$queryPatientPassword = "SELECT Patient_Id FROM Patient WHERE Password = '$password'";
+	$queryDoctorUsername = "SELECT Doctor_Id FROM Doctor WHERE Username = '$username'";
+	$queryDoctorPassword = "SELECT Doctor_Id FROM Doctor WHERE Password = '$password'";
+	$queryHospitalUsername = "SELECT Hospital_Id FROM Patient WHERE Username = '$username'";
+	$queryHospitalPassword = "SELECT Hospital_Id FROM Patient WHERE Password = '$password'";
+
+
+	$hostName = "mysql.eecs.ku.edu";
+	$sqlusername = "bmitchel";
+	$sqlpassword = "hello";
+	$conn = mysql_connect('mysql.eecs.ku.edu','bmitchel','hello') or die('Could not connect: ' . mysql_error());
+
+	mysql_select_db('bmitchel');
+	
+
+
+$query_result = mysql_query( $query, $conn);
+$row = mysql_fetch_array($query_result);
+echo $row['Patient_Id'];
 ?>
